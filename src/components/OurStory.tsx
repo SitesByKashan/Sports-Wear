d69ba19ad11219a64a9a0c264323c0325a57b398
Kashan Malik <sitesbykashan@gmail.com>
2024-10-29 21:14:14 +0500

    .

--- src/components/OurStory.tsx
+++ src/components/OurStory.tsx
@@ -10,7 +10,7 @@ const Ourstory = () => {
           <div className="font-light text-gray-500 sm:text-lg ">
             <h2 className="mb-4 text-4xl tracking-tight font-extrabold wordTheme">We  reinvent the wheel</h2>
             <p className="mb-4">
-              We specialize in premium sportswear designed for athletes and fitness enthusiasts. Our collection combines comfort, durability, and style to help you perform at your best. Whether you're hitting the gym, the field, or the streets, our products are built to match your pace.
+              We specialize in premium sportswear designed for athletes and fitness enthusiasts. Our collection combines comfort, durability, and style to help you perform at your best. Whether you&apos;re hitting the gym, the field, or the streets, our products are built to match your pace.
             </p>
             <p>
               From high-performance activewear to casual sportswear, we deliver quality and innovation. Lightweight, breathable fabrics and modern designs ensure you stay comfortable and look great, no matter the challenge.
